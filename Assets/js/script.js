d359825842eef3ba98e812955a522d4532eb49c8
berhane beyene <monoxica2004@yahoo.com>
2020-10-04 20:35:45 -0500

    Assets/js/script.js modiffied

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,22 +1,30 @@
+$('#burger').on('click', function() {
+    window.open('https://burgeryummy.herokuapp.com/burgers', '_blank')
+});
+
+$('#note').on('click', function() {
+    window.open('https://infonotebook.herokuapp.com/', '_blank')
+});
+
 $('#book').on('click', function() {
     window.open('https://marimartin.github.io/Book-Market/index.html','_blank')
 });
 
 $('#weather').on('click', function() {
-    window.open('https://bbeyenene.github.io/Weather-Forcast/waz-app.html','_blank')
+    window.open('https://bbeyenene.github.io/Weather-Forcast/waz-app.html', '_blank')
 });
 
 $('#pass').on('click', function() {
-    window.open('https://bbeyenene.github.io/Passwor-generator/password.html','_blank')
+    window.open('https://bbeyenene.github.io/Passwor-generator/password.html', '_blank')
 });
 
 $('#work-day').on('click', function() {
-    window.open('https://bbeyenene.github.io/Work-Day-Scheduler/','_blank')
+    window.open('https://bbeyenene.github.io/Work-Day-Scheduler/', '_blank')
 });
 
 $('#quiz').on('click', function() {
-    window.open('https://bbeyenene.github.io/Code-Quize/','_blank')
+    window.open('https://bbeyenene.github.io/Code-Quize/', '_blank')
 });
 $('#team').on('click', function() {
-    window.open('https://bbeyenene.github.io/Employee-Information-Template-Engine/output/team.html','_blank')
+    window.open('https://bbeyenene.github.io/Employee-Information-Template-Engine/output/team.html', '_blank')
 });
